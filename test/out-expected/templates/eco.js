39e9519fffd7635001238fa508ff550ae98011f2
João Bolila <joao@bolila.com>
2013-09-23 16:24:10 +0100

    Compiles into JavaScript functions for use in client-side (sub. grunt-eco)

---
+++ test/out-expected/templates/eco.js
@@ -0,0 +1,60 @@
+if (!window.JST) {
+  window.JST = {};
+}
+window.JST['templates/eco.js'] = function (__obj) {
+  if (!__obj) __obj = {};
+  var __out = [], __capture = function(callback) {
+    var out = __out, result;
+    __out = [];
+    callback.call(this);
+    result = __out.join('');
+    __out = out;
+    return __safe(result);
+  }, __sanitize = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else if (typeof value !== 'undefined' && value != null) {
+      return __escape(value);
+    } else {
+      return '';
+    }
+  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+  __safe = __obj.safe = function(value) {
+    if (value && value.ecoSafe) {
+      return value;
+    } else {
+      if (!(typeof value !== 'undefined' && value != null)) value = '';
+      var result = new String(value);
+      result.ecoSafe = true;
+      return result;
+    }
+  };
+  if (!__escape) {
+    __escape = __obj.escape = function(value) {
+      return ('' + value)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;');
+    };
+  }
+  (function() {
+    (function() {
+    
+      __out.push('Hello, ');
+    
+      __out.push(__sanitize(this.name));
+    
+      __out.push('.\nI\'M SHOUTING AT YOU, ');
+    
+      __out.push(__sanitize(this.name.toUpperCase()));
+    
+      __out.push('!\n');
+    
+    }).call(this);
+    
+  }).call(__obj);
+  __obj.safe = __objSafe, __obj.escape = __escape;
+  return __out.join('');
+}
+
